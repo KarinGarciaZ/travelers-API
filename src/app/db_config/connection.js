c1cf8e2b9aad82169561ece4ca3c0a9efff4b44a
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-07-16 20:38:58 -0500

    Set up connection to database with sequelize.

---
+++ src/app/db_config/connection.js
@@ -0,0 +1,8 @@
+import Sequelize from 'sequelize'
+const sequelize = new Sequelize( process.env.DB_DATABASE, process.env.DB_USER, '', {
+  host: process.env.DB_HOST,
+  dialect: 'mysql',
+  port: process.env.DB_PORT
+});
+console.log('DB NAME: ' + process.env.DB_DATABASE + ' USER NAME: ' + process.env.DB_USER + ' DB PASSWORD: ' + process.env.DB_PASSWORD + ' DB HOST: ' + process.env.DB_HOST + ' DB PORT: ' + process.env.DB_PORT)
+module.exports = sequelize;
