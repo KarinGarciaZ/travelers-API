452e81c891538cd1a15f50ce6a214511f256a4ce
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-07-17 20:54:37 -0500

    Added models routing and required host and port.

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 'use strict'
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import env from 'dotenv'
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const env = require('dotenv')
 env.config();
-const { Cities } = require('./src/app/db_config/schema-db')
 
 const app = express()
 
 app.use(bodyParser.json())
-
 app.use(cors())
 
-app.listen(5000, '127.0.0.1', () => {
+const modelsRouting = require('./src/app/models/models.routing')
+app.use(modelsRouting)
+
+const { host, port } = require('./enviroment')
+app.listen(port, host, () => {
   console.log(`API ready to get requests...`)
-  console.log('running on port ')
+  console.log('running on port ', port)
 })
