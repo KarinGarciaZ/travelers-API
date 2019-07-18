e3e47ac7b8fb712d54590db30b9ea38347bd1480
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-07-17 20:56:41 -0500

    schema finished.

--- src/app/db_config/schema-db.js
+++ src/app/db_config/schema-db.js
@@ -1,83 +1,117 @@
-import Sequelize from 'sequelize'
+const Sequelize = require('sequelize')
 const sequelize = require('./connection')
 
 /*-----------CREATING MODELS----------- */
 
-const Cities = sequelize.define('cities', {
+
+const City = sequelize.define('cities', {
   name: Sequelize.STRING,
+  latitude: Sequelize.DECIMAL(9, 6),
+  longitude: Sequelize.DECIMAL(9, 6),
   statusItem: Sequelize.INTEGER
 })
 
-// const Construction = sequelize.define('construction', {
-//   title: Sequelize.STRING,
-//   description: Sequelize.STRING(2000),
-//   statusConstruction: Sequelize.STRING(20),
-//   address: Sequelize.STRING,
-//   city: Sequelize.STRING,
-//   state: Sequelize.STRING,
-//   startDate: Sequelize.DATE,
-//   finishDate: Sequelize.DATE,
-//   statusItem: Sequelize.INTEGER
-// })
+const Country = sequelize.define('countries', {
+  name: Sequelize.STRING,
+  latitude: Sequelize.DECIMAL(9, 6),
+  longitude: Sequelize.DECIMAL(9, 6),
+  statusItem: Sequelize.INTEGER
+})
 
-// const Service = sequelize.define('service', {
-//   name: Sequelize.STRING,
-//   image: Sequelize.STRING,
-//   description: Sequelize.STRING(2000),
-//   statusItem: Sequelize.INTEGER
-// })
+const Range = sequelize.define('ranges', {
+  name: Sequelize.STRING,
+  medal: Sequelize.STRING,
+  statusItem: Sequelize.INTEGER
+})
 
-// const User = sequelize.define('user', {
-//   username: {
-//     type: Sequelize.STRING,
-//     unique: true
-//   },
-//   name: Sequelize.STRING,
-//   email: {
-//     type: Sequelize.STRING,
-//     validate: { isEmail: true },
-//     unique: true
-//   },
-//   phoneNumber: Sequelize.STRING,
-//   password: Sequelize.STRING,
-//   statusItem: Sequelize.INTEGER
-// });
+const Album = sequelize.define('albums', {
+  imagesNumber: Sequelize.INTEGER,
+  statusItem: Sequelize.INTEGER
+})
 
-// const Image = sequelize.define('image', {
-//   url: Sequelize.STRING(1000),
-//   statusItem: Sequelize.INTEGER,
-//   mainImage: Sequelize.INTEGER
-// })
+const Image = sequelize.define('images', {
+  url: Sequelize.STRING,
+  statusItem: Sequelize.INTEGER
+})
 
-// const Type = sequelize.define('type', {
-//   name: {
-//     type: Sequelize.STRING,
-//     unique: true
-//   },
-//   statusItem: Sequelize.INTEGER
-// });
+const Like = sequelize.define('likes', {
+  statusItem: Sequelize.INTEGER
+})
 
-// const Code = sequelize.define('code', { 
-//   statusItem: Sequelize.INTEGER,
-//   code: Sequelize.INTEGER
-// })
+const Comment = sequelize.define('comments', {
+  text: Sequelize.STRING,
+  statusItem: Sequelize.INTEGER
+})
 
+const User = sequelize.define('users', {
+  name: Sequelize.STRING,
+  email: {
+    type: Sequelize.STRING,
+    validate: { isEmail: true },
+    unique: true
+  }, 
+  username: {
+    type: Sequelize.STRING,
+    unique: false
+  }, 
+  password: Sequelize.STRING,
+  statusItem: Sequelize.INTEGER
+})
+
+const UserInfo = sequelize.define('userinfo', {
+  biography: Sequelize.STRING,
+  phone: Sequelize.STRING,
+  gender: Sequelize.STRING,
+  website: Sequelize.STRING
+})
+
+const Follow = sequelize.define('follows', {
+  idFollowing: Sequelize.INTEGER,
+  statusItem: Sequelize.INTEGER
+})
 
 /*-----------CREATING RELATIONS----------- */
 
-// Construction.belongsTo( Type );
-// Type.hasMany(Construction);
-// Image.belongsTo(Construction);
-// Construction.hasMany(Image);
-// Code.belongsTo(User);
-// User.hasMany(Code);
+Range.hasMany(Country)
+Range.hasMany(City)
+
+City.belongsTo(Range)
+City.belongsTo(Country)
+City.hasMany(Album)
+
+Country.belongsTo(Range)
+Country.hasMany(City)
+
+Album.belongsTo(User)
+Album.belongsTo(City)
+Album.hasMany(Like)
+Album.hasMany(Image)
+Album.hasMany(Comment)
+
+Image.belongsTo(Album)
+
+Like.belongsTo(User)
+Like.belongsTo(Album)
+
+Comment.belongsTo(User)
+Comment.belongsTo(Album)
+
+User.hasMany(Album)
+User.hasMany(Like)
+User.hasMany(Comment)
+User.hasMany(Follow)
+User.hasOne(UserInfo)
+
+UserInfo.belongsTo(User)
+
+Follow.belongsTo(User)
 
 
 /*--GENERETE TABLES AND RELATIONS IF THESE DOESN'T EXIST--*/
 
-sequelize.sync()
+sequelize.sync(/*{force: true}*/)
 .then( () => console.log('DATABASE READY TO WORK'))
 .catch( error => console.log('ERROR CONNECTING TO THE DATABASE: --->', error))
 
 
-export default { Cities };
+module.exports = { City, Range, Country }
