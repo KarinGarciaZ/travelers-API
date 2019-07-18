e69a5fface66890a2830c83da133554476b7a83d
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-07-17 20:56:27 -0500

    Updated connection.

--- src/app/db_config/connection.js
+++ src/app/db_config/connection.js
@@ -1,5 +1,5 @@
-import Sequelize from 'sequelize'
-const sequelize = new Sequelize( process.env.DB_DATABASE, process.env.DB_USER, '', {
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize( process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'mysql',
   port: process.env.DB_PORT
