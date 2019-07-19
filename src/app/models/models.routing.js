7c781a8206f0ea031a2448dba8746d359fa26987
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-07-19 15:46:43 -0500

    Added image model.

--- src/app/models/models.routing.js
+++ src/app/models/models.routing.js
@@ -6,11 +6,13 @@ const Range = require('./Range/range.routing')
 const Country = require('./Country/country.routing')
 const User = require('./User/user.routing')
 const Album = require('./Album/album.routing')
+const Image = require('./Image/image.routing')
 
 router.use('/city', City)
 router.use('/range', Range)
 router.use('/country', Country)
 router.use('/user', User)
 router.use('/album', Album)
+router.use('/image', Image)
 
 module.exports = router;
