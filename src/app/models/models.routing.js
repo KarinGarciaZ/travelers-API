cbf5e2829c8a178e7abc90dacd916e861a70c4a0
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-07-18 20:04:16 -0500

    Routing added to models admin.

--- src/app/models/models.routing.js
+++ src/app/models/models.routing.js
@@ -3,8 +3,12 @@ const router = express.Router()
 
 const City = require('./City/city.routing')
 const Range = require('./Range/range.routing')
+const Country = require('./Country/country.routing')
+const USer = require('./User/user.routing')
 
 router.use('/city', City)
 router.use('/range', Range)
+router.use('/country', Country)
+router.use('/user', USer)
 
 module.exports = router;
