73dcf21c0ef8828829476ce32897f8e3a477b428
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-07-23 10:09:10 -0500

    Added field to album model.

--- src/app/db_config/schema-db.js
+++ src/app/db_config/schema-db.js
@@ -26,6 +26,7 @@ const Range = sequelize.define('ranges', {
 
 const Album = sequelize.define('albums', {
   imagesNumber: Sequelize.INTEGER,
+  description: Sequelize.STRING,
   statusItem: Sequelize.INTEGER
 })
 
